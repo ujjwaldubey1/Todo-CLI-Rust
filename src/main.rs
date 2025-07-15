3ff19bf5435e1e3cae0399ce4603c6933220143d
Ujjwal <ujjwal07dubey@gmail.com>
2025-07-15 20:53:15 +0530

    add the functionality of saving input to the file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-use std::io;
-fn main() {
-    println!("Please write somthing !");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Error in reading input");
-    print!("{}", input);
-    print!("{}", input);
+use std::fs::OpenOptions;
+use std::io::{self, Write};
+
+fn main() -> std::io::Result<()> {
+    let user_input = take_input();
+    save_to_file(&user_input, "task.txt");
+    print!("Data is stored!");
+    Ok(())
 }
+
+// fn take_input() -> String {
+//     println!("Please write somthing !");
+//     let mut input = String::new();
+//     io::stdin()
+//         .read_line(&mut input)
+//         .expect("Error in reading input");
+//     input.trim().to_string()
+// }
+
+fn take_input() -> String {
+    println!("enter value");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error occured");
+    input.trim().to_string()
+}
+
+fn save_to_file(data: &str, file_name: &str) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_name)?;
+    writeln!(file, "{}", data)?;
+    Ok(())
+}
+
+// fn save_to_file(data: &str, file_name: &str) -> std::io::Result<()> {
+//     let mut file = OpenOptions::new()
+//         .append(true)
+//         .create(true)
+//         .open(file_name)?;
+//     writeln!(file, "{}", data)?;
+//     Ok(())
+// }
---
+++ src/tempCodeRunnerFile.rs
@@ -0,0 +1,8 @@
+fn take_input() -> String {
+//     println!("Please write somthing !");
+//     let mut input = String::new();
+//     io::stdin()
+//         .read_line(&mut input)
+//         .expect("Error in reading input");
+//     input.trim().to_string()
+// }
