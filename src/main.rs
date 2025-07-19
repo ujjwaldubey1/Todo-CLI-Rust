94080845161462828c2ac912ae20ea598ea790f7
Ujjwal <ujjwal07dubey@gmail.com>
2025-07-19 21:44:18 +0530

    find the all the done tasks functionality added

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,25 @@ fn main() -> std::io::Result<()> {
         let user_todo_input = take_input();
         save_to_file(&user_todo_input, &file_path);
         print!("Data is stored!");
-        
     } else if _command_input.trim() == "Get tasks" {
         let taks = read_todo_file(&file_path);
         println!("Your tasks are \n {}", taks);
+    } else if _command_input.trim() == "mark done" {
+        println!("Enter the Id of task");
+        let mut id_input = String::new();
+        io::stdin().read_line(&mut id_input).expect("Not valid id");
+
+        let id: i32 = match id_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                print!("invalid number");
+                return Ok(());
+            }
+        };
+
+        mark_done(id, &file_path);
+    } else if _command_input.trim() == "done tasks" {
+        show_done_tasks(&file_path);
     }
 
     Ok(())
@@ -48,13 +63,17 @@ fn take_input() -> String {
 }
 
 fn save_to_file(data: &str, file_name: &str) -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(file_name)?;
-    let mut tasks: Vec<Task> = vec![];
+    // let mut file = OpenOptions::new()
+    //     .append(true)
+    //     .create(true)
+    //     .open(file_name)?;
+    let mut tasks: Vec<Task> = if let Ok(content) = fs::read_to_string(file_name) {
+        serde_json::from_str(&content).unwrap_or_else(|_| vec![])
+    } else {
+        vec![]
+    };
 
-    let new_id = tasks.iter().map(|t| t.id).max().unwrap_or(0);
+    let new_id = tasks.iter().map(|t| t.id).max().unwrap_or(0) + 1;
 
     tasks.push(Task {
         id: new_id,
@@ -62,7 +81,7 @@ fn save_to_file(data: &str, file_name: &str) -> std::io::Result<()> {
         done: false,
     });
     let json = serde_json::to_string_pretty(&tasks).expect("serialization faild");
-    writeln!(file, "{:}", json)?;
+    fs::write(file_name, json)?;
 
     Ok(())
 }
@@ -79,4 +98,61 @@ fn save_to_file(data: &str, file_name: &str) -> std::io::Result<()> {
 fn read_todo_file(file: &str) -> String {
     let todos_from_file = fs::read_to_string(file).expect("Error in reading");
     todos_from_file
+}
+
+fn mark_done(id: i32, file: &str) -> std::io::Result<()> {
+    let mut tasks: Vec<Task> = if let Ok(content) = fs::read_to_string(file) {
+        serde_json::from_str(&content).unwrap_or_else(|_| vec![])
+    } else {
+        vec![]
+    };
+
+    if let Some(task) = tasks.iter_mut().find(|t| t.id == id) {
+        task.done = true;
+    }
+    let json = serde_json::to_string_pretty(&tasks)?;
+    fs::write(file, json)?;
+    Ok(())
+}
+
+fn show_done_tasks(file_path: &str) -> std::io::Result<()> {
+    let tasks: Vec<Task> = if let Ok(content) = fs::read_to_string(file_path) {
+        serde_json::from_str(&content).unwrap_or_else(|_| vec![])
+    } else {
+        vec![]
+    };
+
+    let mut find_done_task = false;
+
+    for task in tasks {
+        if task.done {
+            print!("id {}, title {}", task.id, task.title);
+            find_done_task = true;
+        }
+    }
+
+    if !find_done_task {
+        print!("Taks did't found");
+    }
+    
+
+    // let tasks: Vec<Task> = if let Ok(content) = fs::read_to_string(file_path) {
+    //     serde_json::from_str(&content).unwrap_or_else(|_| vec![])
+    // } else {
+    //     vec![]
+    // };
+
+    // let mut found_done_task = false;
+    // for task in tasks {
+    //     if task.done {
+    //         print!("ID:{} , Title {}", task.id, task.title);
+    //         found_done_task = true;
+    //     }
+    // }
+
+    // if !found_done_task {
+    //     print!("No done task found");
+    // }
+
+    Ok(())
 }
